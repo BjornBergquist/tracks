7d40c950dae35a8221c650c6009c5062ea671b5c
Björn Bergquist <bjorn.bergquist@codic.se>
2021-10-27 07:45:43 +0200

    Added props and state to inputs

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,17 +1,33 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { StyleSheet, View } from 'react-native'
 import {Text, Input, Button} from 'react-native-elements'
 import Spacer from '../components/Spacer'
 
 const SignupScreen = ({navigation}) => {
-    return (
-        <View style={styles.container}>
-            <Spacer><Text h3>Sign Up for Tracker</Text></Spacer>
-            <Spacer><Input label="Password" /></Spacer>
-            <Spacer><Input label="Email"/></Spacer>
-            <Spacer><Button title="Sign Up"/></Spacer>
-        </View>
-    )
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  return (
+    <View style={styles.container}>
+      <Spacer><Text h3>Sign Up for Tracker</Text></Spacer>
+      <Spacer><Input 
+        label="Email" 
+        value={email} 
+        onChangeText={setEmail} 
+        autoCapitalize="none" 
+        autoCorrect={false}
+      /></Spacer>
+      <Spacer><Input 
+        label="Password" 
+        value={password} 
+        onChangeText={setPassword} 
+        autoCapitalize="none" 
+        autoCorrect={false}
+        secureTextEntry
+      /></Spacer>
+      <Spacer><Button title="Sign Up"/></Spacer>
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
