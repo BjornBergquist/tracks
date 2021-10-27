d8dbbde66b2fa604e1ad845482043a53cf71095b
Björn Bergquist <bjorn.bergquist@codic.se>
2021-10-27 11:23:47 +0200

    Created UI for SigninScreen

--- src/screens/SigninScreen.js
+++ src/screens/SigninScreen.js
@@ -1,14 +1,40 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, {useContext} from 'react'
+import { StyleSheet, View } from 'react-native'
+import { Context as AuthContext} from '../context/AuthContext'
+import AuthForm from '../components/AuthForm'
+import NavLink from '../components/NavLink'
 
 const SigninScreen = () => {
+    const {state, signin} = useContext(AuthContext)
+
     return (
-        <View>
-            <Text style={{ fontSize: 48}}>SigninScreen</Text>
+        <View style={styles.container}>
+        <AuthForm 
+            headerText="Sign In to Your Account" 
+            errorMessage={state.errorMessage} 
+            onSubmit={signin} 
+            submitButtonText="Sign In" 
+        />
+        <NavLink 
+            routeName="Signup"
+            text="Don't have an account? Sign up instead!"
+        />
         </View>
     )
 }
 
-export default SigninScreen
+const styles = StyleSheet.create({
+    container:{
+        flex: 1, 
+        justifyContent: 'center',
+        marginBottom: 200,
+    },
+})
 
-const styles = StyleSheet.create({})
+SigninScreen.navigationOptions = () => {
+    return {
+        headerShown: false,
+    };
+};
+
+export default SigninScreen
