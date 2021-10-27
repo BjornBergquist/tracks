e4317019fb4a920a019ec0522775cc132e41979c
Björn Bergquist <bjorn.bergquist@codic.se>
2021-10-27 13:02:02 +0200

    Changing screen will now clear error message

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -7,12 +7,15 @@ const authReducer = (state, action) => {
     switch (action.type){
         case 'add_error':
             return {...state, errorMessage: action.payload}
-        case 'signup':
+        case 'signin':
             return { errorMessge: '', token: action.payload}
+        case 'clear_error_message':
+            return {...state, errorMessage: ''}
         default: 
             return state
     }
 }
+
 
 const signup = (dispatch) => async ({email, password}) => {
     // make api request to sign up with that email and password
@@ -20,25 +23,45 @@ const signup = (dispatch) => async ({email, password}) => {
         // if we sign up, modify our state, and say that we are authenticated
         const response = await TrackerApi.post('/signup', {email, password})
         await AsyncStorage.setItem('token', response.data.token)
-        dispatch({type: 'signup', payload: response.data.token})
+        dispatch({type: 'signin', payload: response.data.token})
         
-        navigate('mainFlow', null)
+        navigate('TrackList')
     } catch (err) {
         // if signing up fails, we probably need to reflect an error message
         // somewhere 
-
-        dispatch({type: 'add_error', payload: 'Something went wrong with sign up'})
-        console.log(err.response.data)
+        dispatch({
+            type: 'add_error', 
+            payload: 'Something went wrong with sign up'
+        })
     }
 }
 
-
-const signin = (dispatch) => {
-    return ({email, password}) => {
-        // Try to signin
+const signin = (dispatch) => async ({email, password}) => {
+    // Try to signin
+    try {
         // Handle success by updating state
+        const response = await TrackerApi.post('/signin', {email, password})
+        await AsyncStorage.setItem('token', response.data.token)
+        dispatch({type: 'signin', payload: response.data.token})
+        navigate('TrackList')
+    } catch (err) {
         // Handle failure by showing error message (somehow)
+        dispatch({
+            type: 'add_error', 
+            payload: 'Something went wrong with sign in'
+        })
     }
+}
+
+const tryLocalSignin = dispatch => async () => {
+    const token = await AsyncStorage.getItem('token')
+    if (token) {
+        dispatch({type: 'signin', payload: token})
+        navigate('TrackList')
+    } else {
+        navigate('loginFlow')
+    }
+
 }
 
 const signout = (dispatch) => {
@@ -47,8 +70,12 @@ const signout = (dispatch) => {
     }
 }
 
+const clearErrorMessage = dispatch => () => {
+    dispatch({type: 'clear_error_message'})
+}
+
 export const {Provider, Context } = createDataContext(
     authReducer,
-    {signup, signin, signout},
+    {signup, signin, signout, clearErrorMessage, tryLocalSignin},
     { token: null, errorMessage: ''}
 )
--- src/screens/SigninScreen.js
+++ src/screens/SigninScreen.js
@@ -1,24 +1,28 @@
 import React, {useContext} from 'react'
 import { StyleSheet, View } from 'react-native'
+import { NavigationEvents } from 'react-navigation'
 import { Context as AuthContext} from '../context/AuthContext'
 import AuthForm from '../components/AuthForm'
 import NavLink from '../components/NavLink'
 
 const SigninScreen = () => {
-    const {state, signin} = useContext(AuthContext)
+    const {state, signin, clearErrorMessage} = useContext(AuthContext)
 
     return (
         <View style={styles.container}>
-        <AuthForm 
-            headerText="Sign In to Your Account" 
-            errorMessage={state.errorMessage} 
-            onSubmit={signin} 
-            submitButtonText="Sign In" 
-        />
-        <NavLink 
-            routeName="Signup"
-            text="Don't have an account? Sign up instead!"
-        />
+            <NavigationEvents 
+                onWillFocus={clearErrorMessage}
+            />
+            <AuthForm 
+                headerText="Sign In to Your Account" 
+                errorMessage={state.errorMessage} 
+                onSubmit={signin} 
+                submitButtonText="Sign In" 
+            />
+            <NavLink 
+                routeName="Signup"
+                text="Don't have an account? Sign up instead!"
+            />
         </View>
     )
 }
