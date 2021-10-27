3f0925cc00fa23b1ed129ddfdacf2683977b7eb2
Björn Bergquist <bjorn.bergquist@codic.se>
2021-10-27 10:48:57 +0200

    Updated api baseURL
    Test server only last for 2 hours.

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export default axios.create({
-    baseURL: 'http://c591-92-32-66-116.ngrok.io'  // Needs to be updated after 2 hours.
+    baseURL: 'http://ee96-92-32-66-116.ngrok.io'  // Needs to be updated after 2 hours.
 })
