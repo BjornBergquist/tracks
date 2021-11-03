cb8bfb9bd2abcbe516b6f4f8e35441cae6621a1b
Björn Bergquist <bjorn.bergquist@codic.se>
2021-11-03 08:06:06 +0100

    Now collecting user location, with mocked data

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,7 +1,8 @@
+import '../_mockLocation'
 import React,{useEffect, useState} from 'react'
 import { StyleSheet } from 'react-native'
 import { Text } from 'react-native-elements'
-import { requestForegroundPermissionsAsync } from 'expo-location'; 
+import { requestForegroundPermissionsAsync, watchPositionAsync, Accuracy } from 'expo-location'; 
 import Map from '../components/Map'
 import { SafeAreaView } from 'react-navigation'
 
@@ -14,6 +15,13 @@ const TrackCreateScreen = () => {
           if (!granted) {
             throw new Error('Location permission not granted')
           }
+          await watchPositionAsync({
+            accuracy: Accuracy.BestForNavigation,
+            timeInterval: 1000,
+            distanceInterval: 10
+          }, (location) => {
+            console.log(location);
+          })
         } catch (e) {
             setErr(e)
         }
