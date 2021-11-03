ad0c39f0ca6c6e718b24f46866ceb6dde092c8c8
Björn Bergquist <bjorn.bergquist@codic.se>
2021-11-03 08:44:22 +0100

    Show user location as a circle on the map.

--- src/_mockLocation.js
+++ src/_mockLocation.js
@@ -11,8 +11,8 @@ const getLocation = increment => {
             accuracy: 5,
             altitudeAccuracy: 5,
             altitude: 5,
-            longitude: -122.0312186 + increment * tenMetersWithDegrees,
-            latitude: 37.33233141 + increment * tenMetersWithDegrees
+            longitude: 11.93123490550735 + increment * tenMetersWithDegrees,
+            latitude: 57.69321310339281 + increment * tenMetersWithDegrees
         }
     }
 }
