297cf5af5bc15ac97ae17f56efb7c459afa1af61
Björn Bergquist <bjorn.bergquist@codic.se>
2021-11-03 07:50:17 +0100

    Updated baseURL

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,5 +1,7 @@
 import axios from 'axios'
 
 export default axios.create({
-    baseURL: 'http://807a-92-32-66-116.ngrok.io'  // Needs to be updated after 2 hours.
+    baseURL: 'http://2559-92-32-66-116.ngrok.io'  // Needs to be updated after 2 hours.
 })
+
+// ngrok http 3000
