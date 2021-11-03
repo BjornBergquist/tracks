edaeee682225780c87db99f678daeca2442478e8
Björn Bergquist <bjorn.bergquist@codic.se>
2021-11-03 09:32:42 +0100

    Broke out useLocation from TrackCreateScreen and changed so it only tracks while screen is focused

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,37 +1,16 @@
 import '../_mockLocation'
-import React,{useEffect, useState, useContext} from 'react'
+import React,{useContext} from 'react'
 import { StyleSheet } from 'react-native'
 import { Text } from 'react-native-elements'
-import { requestForegroundPermissionsAsync, watchPositionAsync, Accuracy } from 'expo-location'; 
 import Map from '../components/Map'
-import { SafeAreaView } from 'react-navigation'
+import { SafeAreaView, withNavigationFocus } from 'react-navigation'
 import {Context as LocationContext} from '../context/LocationContext'
+import useLocation from '../hooks/useLocation'
 
-const TrackCreateScreen = () => {
+const TrackCreateScreen = ({ isFocused}) => {
     const {addLocation} = useContext(LocationContext) 
-    const [err, setErr] = useState(null)
     
-    const startWatching = async () => {
-        try {
-          const { granted } = await requestForegroundPermissionsAsync()
-          if (!granted) {
-            throw new Error('Location permission not granted')
-          }
-          await watchPositionAsync({
-            accuracy: Accuracy.BestForNavigation,
-            timeInterval: 1000,
-            distanceInterval: 10
-          }, (location) => {
-            addLocation(location);
-          })
-        } catch (e) {
-            setErr(e)
-        }
-    }
-
-    useEffect(() => {
-        startWatching()
-    }, [])
+    const [err] = useLocation(isFocused, addLocation)
 
     return (
         <SafeAreaView forceInset={{top: 'always'}}>
@@ -42,7 +21,7 @@ const TrackCreateScreen = () => {
     )
 }
 
-export default TrackCreateScreen
+export default withNavigationFocus(TrackCreateScreen)
 
 const styles = StyleSheet.create({})
 
